824171d8b562d55201808675fd8c3a8083607983
sd2604 <shreyadas260406@gmail.com>
2025-07-07 20:45:09 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,18 @@
+# Task 1: Performing basic mathematical operations
+
+a= int(input('Enter the first number:' ))
+b= int(input('Enter the second number:' ))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition:',c)
+print('Subtraction:',d)
+print('Multiplication:',e)
+print('Division:',f)
+
+# Task 2: Creating personalized greeting
+n1=input('Enter first name:')
+n2=input('Enter last name:')
+n=n1+n2
+print('Hello ',n,'! Welcome to the python program.')
